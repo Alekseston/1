fafa90bae84f9a57d84d7cbc63040019927104a5
Alekseston <7azbuka@gmail.com>
2022-01-21 13:56:58 +0100

    Python_DZ 1-3 in .py

---
+++ Python_DZ1.py
@@ -0,0 +1,24 @@
+a = str
+b = int
+c = float
+d = bytes
+e = list
+f = tuple
+g = list
+h = frozenset
+j = dict
+
+print('a=',a,'b=',b,'c=',c,'d=',d,'e=',e,'f=',f,'g=',g,'h=',h,'j=',j)
+
+s1 = str('Anas')
+s2 = str('tasiya')
+s = s1 + s2
+print(s)
+b = 2
+print(s,b)
+print(s +' '+str(b))
+
+
+
+
+
